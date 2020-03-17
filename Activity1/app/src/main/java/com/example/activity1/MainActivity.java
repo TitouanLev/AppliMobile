0e4c6d98895c0ac96be217682a97fe8f0d31851a
TitouanLev <50668278+TitouanLev@users.noreply.github.com>
2020-03-17 12:22:04 +0100

    First Activity

---
+++ Activity1/app/src/main/java/com/example/activity1/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.activity1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Text text = findViewById(R.id.keywords);
+        Spinner foods = findViewById(R.id.foodTypes);
+        SeekBar results = findViewById(R.id.resultsNumber);
+        Button submit = findViewById(R.id.submit);
+
+        
+    }
+}
