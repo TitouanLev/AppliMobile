bc39a1ad43af0e3c79f4c7455b064064fa7566cd
MatthieuL <leproux.matthieu@gmail.com>
2020-03-24 11:09:36 +0100

    Je tente des choses avec l'api

--- Activity1/app/src/main/java/com/example/activity1/MainActivity.java
+++ Activity1/app/src/main/java/com/example/activity1/MainActivity.java
@@ -5,13 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextPaint;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.Spinner;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.textfield.TextInputEditText;
+
+import org.json.JSONObject;
 
 //API KEY : 34b36018b4b34d719015a96804e28700
 
@@ -30,9 +39,38 @@ public class MainActivity extends AppCompatActivity {
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent truc = new Intent(getApplicationContext(), Activity3.class);
-                startActivity(truc);
+                Intent Act3 = new Intent(getApplicationContext(), Activity3.class);
+                startActivity(Act3);
             }
         });
+
+        // API request
+
+        String URL = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=apples,+flour,+sugar&number=2";
+
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+
+        JsonObjectRequest objectRequest = new JsonObjectRequest(
+                Request.Method.GET,
+                URL,
+                null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.e("API response", response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("API response (err)", error.toString());
+                    }
+                }
+
+
+        );
+
+
     }
 }
